36d5b8b583f75b83b546e431490b026df4b72d48
Kulietta <105567256+Kulietta@users.noreply.github.com>
2022-06-24 10:23:47 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+const myName = "Juliet Ada Okolie";
+const myHeight = "5ft 4inches";
+const myCountry = "Nigeria";
+
+alert("My Name is " + myName+"\nMy Height is " + myHeight+"\nMy Country is " + myCountry)
